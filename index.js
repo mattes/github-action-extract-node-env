dde7908ac0179359b0c5ee5f241834a0bcc16dcb
Matthias Kadenbach <matthias.kadenbach@gmail.com>
2020-09-24 23:40:56 -0700

    add 'set_env' flag

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 console.log("::set-output name=value::" + process.env[ process.env["INPUT_KEY"] ])
 
+if(process.env["INPUT_SET_ENV"] == "true") {
+  console.log("::set-env name=" + process.env["INPUT_KEY"] + "::" + process.env[ process.env["INPUT_KEY"] ])
+}
+
